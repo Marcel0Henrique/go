0966e6ee1fad411c9ce92ec4e9809b9471e6d847
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-10-12 16:14:15 -0300

    update

---
+++ 001_fundamentos_da_linguagem/007_heranca/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+//* Herança
+//? Não existe herança no GO, essa é a forma mais proxima disso
+
+type pessoa struct {
+	nome  string
+	idade int8
+}
+
+// forma mais proxima de  realiza ruma herança
+type estudante struct {
+	pessoa // "Herança"
+	curso  string
+}
+
+func main() {
+	p1 := pessoa{nome: "joão", idade: 15}
+	e1 := estudante{p1, "T.I"}
+	e2 := estudante{pessoa{"Marcos", 26}, "Português"}
+
+	fmt.Println(p1, e1, e2)
+}
