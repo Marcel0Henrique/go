d2c4a2f14000db59a1785750f7a8d7d1be27703b
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-10-13 20:53:17 -0300

    update

--- 001_fundamentos_da_linguagem/011_maps/main.go
+++ 001_fundamentos_da_linguagem/011_maps/main.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
+//* Maps
 func main() {
 
+	// criando map
+	// dentro do "[]" é o tipo da chave, fora é o tipo do valor
+	usuarios := map[string]string{
+		"nome":      "pedro",
+		"sobrenome": "silva",
+	}
+
+	delete(usuarios, "sobrenome")
+	usuarios["signo"] = "Virgem"
+
+	fmt.Println(usuarios, usuarios["nome"])
 }
