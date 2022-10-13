3773e1bbcacde6946b0cac165aa0675c1a8fc603
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-10-13 19:26:20 -0300

    update

--- 001_fundamentos_da_linguagem/010_array_internos/main.go
+++ 001_fundamentos_da_linguagem/010_array_internos/main.go
@@ -1,6 +1,13 @@
 package main
 
+import "fmt"
+
 //* Arrays Internos
 func main() {
+	slice := make([]int, 2, 5)
+
+	fmt.Println(slice)
+	fmt.Println(len(slice)) // Tamanho
+	fmt.Println(cap(slice)) // Capacidade
 
 }
