603319bff385500378ea3b42032db23868e6163d
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-10-15 20:52:02 -0300

    update

--- 001_fundamentos_da_linguagem/023_funcao_init/main.go
+++ 001_fundamentos_da_linguagem/023_funcao_init/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+// Variável global
+var n int
+
+//* Função Init
+//? Função init é executada antes da função main
+//? Função init pode ser uma por arquivo diferente da main
+func init() {
+	fmt.Println("Executando função init")
+	n = 10
+}
+
+func main() {
+	fmt.Println("Função main sendo executada")
+	fmt.Println("Variável global: ", n)
 }
