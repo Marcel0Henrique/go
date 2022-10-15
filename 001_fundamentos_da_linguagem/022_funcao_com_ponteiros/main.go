4ddcfed277088ef3b3af489f0a3c570c74e79bf6
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-10-15 20:13:07 -0300

    update

--- 001_fundamentos_da_linguagem/022_funcao_com_ponteiros/main.go
+++ 001_fundamentos_da_linguagem/022_funcao_com_ponteiros/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+//* Função com ponteiros
+func main() {
+	numero := 20
+	numeroInvertido := InverterSinal(numero)
+	fmt.Println(numeroInvertido)
+
+	num := 40
+	InverterSinalComPonteiro(&num)
+	fmt.Println(num)
+}
+
+func InverterSinalComPonteiro(num *int) {
+	*num = *num * -1
+}
+
+func InverterSinal(num int) int {
+	return num * -1
 }
