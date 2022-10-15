8056be07dea48b61abb9746f0c906f984742b2ae
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-10-14 23:36:13 -0300

    update

--- 001_fundamentos_da_linguagem/016_funcoes_variaticas/main.go
+++ 001_fundamentos_da_linguagem/016_funcoes_variaticas/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+//* Função variaticas
+//? pode receber vários parâmetros por padrão
+func main() {
+	totalSoma := soma(1, 2, 3, 4, 5, 6, 7, 8, 9)
+	fmt.Println(totalSoma)
+}
+
+// função vai receber vários parâmetros int
+func soma(numeros ...int) (total int) {
+	total = 0
+	for _, numero := range numeros {
+		total += numero
+	}
+	return
 }
