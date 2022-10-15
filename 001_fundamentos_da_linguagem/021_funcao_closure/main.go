5372ad791966f4055cf9414aa3c1f1849079dd8e
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-10-15 18:45:36 -0300

    update

--- 001_fundamentos_da_linguagem/021_funcao_closure/main.go
+++ 001_fundamentos_da_linguagem/021_funcao_closure/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+//* Função closure
+//? Basicamente são funções que referenciam variáveis que estão
+//? fora do seu corpo
+func main() {
+	texto := "Dento da função main"
+	fmt.Println(texto)
+
+	funcaoNova := closure()
+	funcaoNova()
+}
+
+func closure() (funcao func()) {
+	texto := "Dentro da função closure"
+	funcao = func() {
+		fmt.Println(texto)
+	}
+	return
 }
