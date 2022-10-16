c024a217f713639ddb1e18f510bbbec4f479581b
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-10-16 14:31:22 -0300

    update

--- 001_fundamentos_da_linguagem/024_metodos/main.go
+++ 001_fundamentos_da_linguagem/024_metodos/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import "fmt"
 
+//* Métodos
+//? São parecidos com funções mas não são a mesma coisa
+//? método está sempre associado a algo, normalmente a um struct
+func main() {
+	p1 := pessoa{"Marcelo", 19}
+	fmt.Println(p1)
+	p1.mostrarDados()
+
+	p1.aumentarIdade()
+	p1.mostrarDados()
+}
+
+type pessoa struct {
+	nome  string
+	idade uint8
+}
+
+//* Criando método
+//? (p pessoa) vincula ao struct esse método
+//? o "u" é uma variável que é usada para referenciar outros campos do struct pessoa
+func (p pessoa) mostrarDados() {
+	fmt.Printf("\nNome: %s\n", p.nome)
+	fmt.Printf("Idade: %d", p.idade)
+}
+
+//* Criado método para alterar valor dentro da pessoa
+//? se usa um ponteiro e não precisa desfazer a referenciação
+func (p *pessoa) aumentarIdade() {
+	p.idade++
 }
