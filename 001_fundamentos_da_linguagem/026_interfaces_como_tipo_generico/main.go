3eadacd2289df9eab7b0dd993ee6d4a6d2dd3254
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-10-16 16:44:13 -0300

    interface como generica

--- 001_fundamentos_da_linguagem/026_interfaces_como_tipo_generico/main.go
+++ 001_fundamentos_da_linguagem/026_interfaces_como_tipo_generico/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+//* Interface como tipo genérico
+//! cuidado para não virar gambiarra
+func main() {
+	generica(12)
+	generica("Generica")
+	generica(true)
+	generica(18.95)
+	generica(nil)
+}
+
+//Função com interface generica
+func generica(interf interface{}) {
+	fmt.Println(interf)
 }
