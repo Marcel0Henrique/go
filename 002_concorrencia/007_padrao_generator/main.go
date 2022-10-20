13c52e07a3257ebd3f5463c154c6922442486f8e
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-10-20 15:26:27 -0300

    padrao generator

---
+++ 002_concorrencia/007_padrao_generator/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// * Padrão Generator
+func main() {
+	canal := escrever("olá Mundo")
+
+	for i := 0; i < 10; i++ {
+		fmt.Println(<-canal)
+	}
+}
+
+func escrever(texto string) <-chan string {
+	canal := make(chan string)
+
+	go func() {
+		for {
+			canal <- fmt.Sprintf("Valor recebido: %s", texto)
+			time.Sleep(time.Millisecond * 500)
+		}
+	}()
+
+	return canal
+}
