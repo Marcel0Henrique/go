8b35a8371bc53aef24e0e2cf37f92310665b8dc3
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2023-03-05 12:27:52 -0300

    update

--- 001_fundamentos_da_linguagem/001_pacotes/auxiliar/auxiliar.go
+++ 001_fundamentos_da_linguagem/001_pacotes/auxiliar/auxiliar.go
@@ -2,10 +2,9 @@ package auxiliar
 
 import "fmt"
 
-//* função com letra Maiúscula é publica
-
 //? é boa pratica colocar um comentário em cima de uma função publica
 
+//* função com letra Maiúscula é publica
 // Escrever registra uma mensagem na tela
 func Escrever() {
 	fmt.Println("Escrevendo do pacote auxiliar")
